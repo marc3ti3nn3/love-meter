99d6df377532793d37eadb1002695ae997a9950c
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2020-10-27 14:47:15 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,20 @@
 let love = 0
 input.onPinPressed(TouchPin.P0, function () {
     love = randint(0, 100)
-    if (love <= 60) {
-        basic.showLeds(`
-            . # . # .
-            # # # . #
-            # . . # #
-            . # # # .
-            . . # . .
-            `)
-        basic.pause(500)
+    if (love <= randint(0, 20)) {
+        basic.showIcon(IconNames.Angry)
     } else {
-        basic.showLeds(`
-            . # . # .
-            # # # # #
-            # # # # #
-            . # # # .
-            . . # . .
-            `)
-        basic.pause(500)
+        if (love <= randint(21, 40)) {
+            basic.showIcon(IconNames.Sad)
+        } else {
+            if (love <= randint(41, 80)) {
+                basic.showIcon(IconNames.Confused)
+            } else {
+                if (love <= randint(81, 100)) {
+                    basic.showIcon(IconNames.Happy)
+                }
+            }
+        }
     }
     basic.showNumber(love)
     basic.pause(1000)
