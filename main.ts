4a44d36cce23767ffdd2929349c9b3f79c45335e
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2020-11-03 13:09:02 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 let love = 0
 input.onPinPressed(TouchPin.P0, function () {
-    love = randint(0, 100)
-    if (love <= randint(0, 20)) {
+    love = randint(1, 100)
+    if (love <= 20) {
         basic.showIcon(IconNames.Angry)
     } else {
-        if (love <= randint(21, 40)) {
+        if (love <= 40) {
             basic.showIcon(IconNames.Sad)
         } else {
-            if (love <= randint(41, 80)) {
+            if (love <= 80) {
                 basic.showIcon(IconNames.Confused)
             } else {
-                if (love <= randint(81, 100)) {
+                if (love <= 100) {
                     basic.showIcon(IconNames.Happy)
                 }
             }
